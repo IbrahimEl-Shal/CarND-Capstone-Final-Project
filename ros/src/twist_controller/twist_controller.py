0e45b66a86be3ee619cb541da68534992ab6ca1a
Ibrahim EL-Shal <i.hamedelshal@gmail.com>
2019-04-03 16:48:33 +0200

    Update Set PID Parameters

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -6,10 +6,9 @@ import rospy
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
-
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
-        # TODO: Implement
+        # Implemention
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
 	kp = 0.3
@@ -33,17 +32,11 @@ class Controller(object):
 	self.last_time = rospy.get_time()
 
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
-	#return 1., 0., 0.
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
 	if not dbw_enabled:
 		self.throttle_controller.reset()
 		return 0., 0., 0.
 
 	current_vel = self.vel_lpf.filt(current_vel)
-
-	# rospy.logwarn("angular vel: {0}".format(angular_vel))
-	
 	steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
 	vel_error = linear_vel - current_vel
@@ -58,12 +51,9 @@ class Controller(object):
 
 	if linear_vel ==0. and current_vel < 0.1:
 		throttle = 0
-		brake = 700 #N*m - to hold the car in place if we are stopped at a light. Acceleration - 1m/s^2
-
+		brake = 700 #N*m - Makethe car in its place if stopped at Red light and acceleration = 1m/s^2
 	elif throttle < .1 and vel_error < 0:
 		throttle = 0
 		decel = max(vel_error, self.decel_limit)
 		brake = abs(decel) * self.vehicle_mass * self.wheel_radius # Torque N * m
-		
-	
         return throttle, brake, steering
